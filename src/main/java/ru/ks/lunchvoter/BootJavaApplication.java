35be112417a84d23a61df4e75881aad0d5404094
Kostya Samarin <ksamarin@gmail.com>
2023-12-25 11:05:47 +0300

    Use generic name for stub

--- src/main/java/ru/ks/lunchvoter/BootJavaApplication.java
+++ src/main/java/ru/ks/lunchvoter/BootJavaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LunchVoterApplication {
+public class BootJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LunchVoterApplication.class, args);
+        SpringApplication.run(BootJavaApplication.class, args);
     }
 }

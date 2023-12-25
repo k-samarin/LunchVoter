5dd61f69f816e45d04d51175143e0c3bd40669c2
Kostya Samarin <ksamarin@gmail.com>
2023-12-25 11:00:56 +0300

    Initial stub for boot application

---
+++ src/main/java/ru/ks/lunchvoter/LunchVoterApplication.java
@@ -0,0 +1,12 @@
+package ru.ks.lunchvoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchVoterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchVoterApplication.class, args);
+    }
+}
